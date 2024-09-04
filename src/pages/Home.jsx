fcb714058d668e7f2c10c705d546ec12b8792004
RidwanAdebosin <adebosinadewale@gmail.com>
2024-09-04 03:27:23 +0100

    Footer component added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+// import FeatureList from "../components/Features/Features";
+// import Header from "../components/Header/Header";
+// import Hero from "../components/Hero/Hero";
+// import VaccineInfo from "../components/VaccineInfo/VaccineInfo";
+// import TopStories from "../components/TopStories/TopStories";
+// import Scheduling from "../components/Scheduling/Scheduling";
+// import VaccineSupport from "../components/VaccineSupport/VaccineSupport";
+// import ClientReview from "../components/ClientReview/ClientReview";
+// import ReachUs from "../components/ReachUs/ReachUs";
+import Footer from "../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <>
+      {/* <Hero />
+      <FeatureList />
+      <VaccineInfo />
+      <TopStories />
+      <Scheduling />
+      <VaccineSupport />
+      <ClientReview />
+      <ReachUs /> */}
+      <Footer />
+    </>
+  );
+};
+export default Home;
